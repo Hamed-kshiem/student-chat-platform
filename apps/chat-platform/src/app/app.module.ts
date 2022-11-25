04306484c1ed7f6c5577575e5e23bcf8fa450ef2
Hamed Kshiem <32527533+Hamed-kshiem@users.noreply.github.com>
2022-11-25 10:37:20 +0100

    install depencies and mongodb init

--- apps/chat-platform/src/app/app.module.ts
+++ apps/chat-platform/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:rIZ5arJNG4qlTSbw@cluster0.my7gggd.mongodb.net/test'
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
